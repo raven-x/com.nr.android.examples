32932ce5c6ea3ab89e1018be59b9a52f8aad9a15
vvkirillov <v.v.kirillov69@gmail.com>
2015-10-28 15:17:27 +0300

    Endless recycler project modified

--- endlessrecycler/src/main/java/com/example/vkirillov/endlessrecycler/MainActivity.java
+++ endlessrecycler/src/main/java/com/example/vkirillov/endlessrecycler/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vkirillov.endlessrecycler;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,19 +11,23 @@ import com.example.vkirillov.endlessrecycler.view.ILoadListener;
 import com.example.vkirillov.endlessrecycler.view.RecyclerAdapter;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private final List<Model> mModel = new ArrayList<>();
     private RecyclerView mRecyclerView;
     private RecyclerAdapter adapter;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        generate(0, 15);
+        handler = new Handler();
+
+        mModel.addAll(generate(0, 15));
 
         mRecyclerView = (RecyclerView) findViewById(R.id.endlessRecycler);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -33,23 +38,44 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(adapter);
     }
 
-    private void generate(int start, int count){
+    private Collection<Model> generate(int start, int count){
+        List<Model> result = new ArrayList<>(count);
         for(int i=0;i<count;i++){
             Model entry = new Model();
             int value = start * 10 + i;
             entry.setTitle(Integer.toString(value));
             entry.setContent(String.format("Article %d", value));
-            mModel.add(entry);
+            result.add(entry);
         }
+        return result;
     }
 
     private class OnLoadListener implements ILoadListener{
 
         @Override
-        public void onLoad(int currentPage) {
-            generate(currentPage, 10);
-            adapter.setLoaded();
-            adapter.notifyDataSetChanged();
+        public void onLoad(final int currentPage) {
+            //This model_item will be added as progress bar for loading progress notification
+            mModel.add(null);
+            adapter.notifyItemInserted(mModel.size() - 1);
+
+            //Delay loading for 5 seconds
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //Generate a new set of data
+                    Collection<Model> temp = generate(currentPage, 10);
+
+                    //Remove progress model_item
+                    mModel.remove(mModel.size() - 1);
+                    adapter.notifyItemRemoved(mModel.size() - 1);
+
+                    //Update model and view
+                    mModel.addAll(temp);
+                    adapter.setLoaded();
+                    //Not effective but suitable for demonstration
+                    adapter.notifyDataSetChanged();
+                }
+            }, 5000);
         }
     }
 }
--- endlessrecycler/src/main/java/com/example/vkirillov/endlessrecycler/view/RecyclerAdapter.java
+++ endlessrecycler/src/main/java/com/example/vkirillov/endlessrecycler/view/RecyclerAdapter.java
@@ -5,26 +5,35 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.vkirillov.endlessrecycler.R;
 import com.example.vkirillov.endlessrecycler.model.Model;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Adapter for endless recycler adapter
  */
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.EndlessViewHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter {
+    private static final int VIEW_ITEM = 0;
+    private static final int PROGRESS_ITEM = 1;
 
     /**Reference to model*/
     private final List<Model> mModel;
-    private boolean mLoading;
-    /**Then the difference between item total count and current visible item position
+    private AtomicBoolean mLoading;
+    /**Then the difference between model_item total count and current visible model_item position
      * reaches the threshold the new data page loading must be launched (5 by default)*/
     private int mVisibleThreshold = 5;
     private ILoadListener mOnLoadListener;
     private int mCurrentPage;
+
+    @Override
+    public int getItemViewType(int position) {
+        return mModel.get(position) == null ? PROGRESS_ITEM : VIEW_ITEM;
+    }
 
     /**
      * Constructor
@@ -32,6 +41,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Endles
      * @param model data model
      */
     public RecyclerAdapter(RecyclerView recyclerView, List<Model> model) {
+        mLoading = new AtomicBoolean();
         mModel = model;
         recyclerView.addOnScrollListener(new EndlessScrollListener());
     }
@@ -45,10 +55,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Endles
     }
 
     @Override
-    public EndlessViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item, parent, false);
-        return new EndlessViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if(viewType == VIEW_ITEM) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.model_item, parent, false);
+            return new ItemViewHolder(view);
+        }else{
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.progress_item, parent, false);
+            return new ProgressBarViewHolder(view);
+        }
     }
 
     @Override
@@ -57,28 +72,48 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Endles
     }
 
     @Override
-    public void onBindViewHolder(EndlessViewHolder holder, int position) {
-        EndlessViewHolder evh = holder;
-        Model entry = mModel.get(position);
-        evh.title.setText(entry.getTitle());
-        evh.content.setText(entry.getContent());
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof ItemViewHolder) {
+            ItemViewHolder evh = (ItemViewHolder) holder;
+            Model entry = mModel.get(position);
+            evh.title.setText(entry.getTitle());
+            evh.content.setText(entry.getContent());
+        }else{
+            ProgressBarViewHolder pbvh = (ProgressBarViewHolder) holder;
+            pbvh.progressBar.setIndeterminate(true);
+        }
     }
 
     /**
      * Notifies adapter about loading completion
      */
     public void setLoaded(){
-        mLoading = false;
+        mLoading.set(false);
     }
 
-    public static class EndlessViewHolder extends RecyclerView.ViewHolder {
+    /**
+     * Item view holder
+     */
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
         public final TextView title;
         public final TextView content;
 
-        public EndlessViewHolder(View itemView) {
+        public ItemViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.title);
             content = (TextView) itemView.findViewById(R.id.content);
+        }
+    }
+
+    /**
+     * Progress bar view holder
+     */
+    public static class ProgressBarViewHolder extends RecyclerView.ViewHolder{
+        public final ProgressBar progressBar;
+
+        public ProgressBarViewHolder(View itemView) {
+            super(itemView);
+            progressBar = (ProgressBar) itemView.findViewById(R.id.progress);
         }
     }
 
@@ -94,8 +129,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Endles
             totalItemCount = layoutManager.getItemCount();
             lastVisibleItemPos = layoutManager.findLastVisibleItemPosition();
 
-            if(!mLoading && (totalItemCount <= lastVisibleItemPos + mVisibleThreshold)){
-                mLoading = true;
+            if(!mLoading.get() && (totalItemCount <= lastVisibleItemPos + mVisibleThreshold)){
+                mLoading.set(true);
                 if(mOnLoadListener != null){
                     //Load data
                     mOnLoadListener.onLoad(mCurrentPage++);
